79e3422967735a5a2aaca0ad01cda4bc0f3441e0
nikhil <nikhil.k@qburst.com>
2024-03-28 21:23:47 +0530

    Commit -1

--- signuptemp/build.gradle.kts
+++ signuptemp/build.gradle.kts
@@ -60,4 +60,12 @@ dependencies {
     val lifecycleRuntime = "2.6.2"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntime")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleRuntime")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
