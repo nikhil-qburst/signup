3153a114a66f9c9cf1e97219fb3b36c59980021c
nikhil <nikhil.k@qburst.com>
2024-03-29 11:32:59 +0530

    Commit -3 add code to generate pom.xml

--- signuptemp/build.gradle.kts
+++ signuptemp/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id ("maven-publish")
+    id("maven-android-sdk-deployer")
 }
 
 android {
@@ -47,6 +48,36 @@ publishing{
                 artifactId = "signuptemp"
                 version = "1.0.1"
                 from(components["release"])
+                pom {
+                    // Add additional metadata about your library
+                    name.set("Your Library Name")
+                    description.set("Description of your library")
+                    url.set("com.github.nikhil-qburst/signup")
+
+                    // Add dependencies if your library has any
+                     dependencies {
+                         implementation("androidx.core:core-ktx:1.9.0")
+                         implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+                         implementation("androidx.activity:activity-compose:1.8.2")
+                         implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+                         implementation("androidx.compose.ui:ui")
+                         implementation("androidx.compose.ui:ui-graphics")
+                         implementation("androidx.compose.ui:ui-tooling-preview")
+                         implementation("androidx.compose.material3:material3")
+
+                         val lifecycleRuntime = "2.6.2"
+                         implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntime")
+                         implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleRuntime")
+
+                         testImplementation("junit:junit:4.13.2")
+                         androidTestImplementation("androidx.test.ext:junit:1.1.5")
+                         androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+                         androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+                         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+                         debugImplementation("androidx.compose.ui:ui-tooling")
+                         debugImplementation("androidx.compose.ui:ui-test-manifest")
+                     }
+                }
             }
         }
     }
