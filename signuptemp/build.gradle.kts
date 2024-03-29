05ba5a3dde4c942c26d7f8a20ce4f27a0156f9e1
nikhil <nikhil.k@qburst.com>
2024-03-29 22:23:13 +0530

    Commit -5 add pom.xml configuration

--- signuptemp/build.gradle.kts
+++ signuptemp/build.gradle.kts
@@ -47,57 +47,31 @@ publishing{
                 artifactId = "signuptemp"
                 version = "1.0.1"
                 from(components["release"])
-                pom {
-                    name.set("signup-temp")
-                    description.set("Description of your library")
-                    url.set("https://github.com/nikhil-qburst/signup")
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("nikhil-qburst")
-                            name.set("Nikhil kumar")
-                            email.set("nikhil.k@qburst.com")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:https://github.com/nikhil-qburst/signup.git")
-                        developerConnection.set("scm:git:https://github.com/nikhil-qburst/signup.git")
-                        url.set("https://github.com/nikhil-qburst/signup")
-                    }
-
-                    // Add dependencies if your library has any
-                     dependencies {
-                         implementation("androidx.core:core-ktx:1.9.0")
-                         implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-                         implementation("androidx.activity:activity-compose:1.8.2")
-                         implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-                         implementation("androidx.compose.ui:ui")
-                         implementation("androidx.compose.ui:ui-graphics")
-                         implementation("androidx.compose.ui:ui-tooling-preview")
-                         implementation("androidx.compose.material3:material3")
-
-                         val lifecycleRuntime = "2.6.2"
-                         implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntime")
-                         implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleRuntime")
-
-                         testImplementation("junit:junit:4.13.2")
-                         androidTestImplementation("androidx.test.ext:junit:1.1.5")
-                         androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-                         androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-                         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-                         debugImplementation("androidx.compose.ui:ui-tooling")
-                         debugImplementation("androidx.compose.ui:ui-test-manifest")
-                     }
-                }
             }
         }
     }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    val lifecycleRuntime = "2.6.2"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntime")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleRuntime")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
